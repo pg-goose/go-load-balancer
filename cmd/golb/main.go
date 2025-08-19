906d25176e3f175fc708fd89be4cdfc680413f8a
pg-goose <pau.galopa.dev@gmail.com>
2025-08-19 18:43:06 +0200

    Added usage msg

--- cmd/golb/main.go
+++ cmd/golb/main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/pg-goose/go-load-balancer/internal/loadbalancer"
 	lb "github.com/pg-goose/go-load-balancer/internal/loadbalancer"
 )
 
-var confpath = flag.String("confpath", "config.yml", "path to configuration file")
+const USAGE_STR = `golb - tiny go load balancer
+
+Usage:
+  golb [flags]
+
+Flags:`
 
 func main() {
+	// custom help message
+	flag.Usage = func() {
+		out := flag.CommandLine.Output()
+		fmt.Fprintln(out, USAGE_STR)
+		flag.PrintDefaults()
+	}
+	confpath := flag.String("confpath", "config.yml", "path to configuration file")
+	flag.Parse()
+
+	// start the load balancer
 	config := loadbalancer.LoadConfig(*confpath)
 	lb := lb.NewLoadBalancer(config)
+
 	defer lb.Stop()
 	log.Fatal(lb.Start())
 }
